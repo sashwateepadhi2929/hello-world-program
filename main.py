2c91dbc1d6cca4a4d4bd6273f272444ce81cdec8
sashwateepadhi2929 <94121377+sashwateepadhi2929@users.noreply.github.com>
2021-11-11 19:29:35 +0530

    changed name
    small change in print statement

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('ayush sharma ji')
+    print_hi('sashwateepadhi2929')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
